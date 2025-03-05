1df0d2d52557f8d5c6d479df6506b36959f33cf6
Fergal Riordan <riordanf@tcd.ie>
2025-03-05 17:29:05 +0000

    Reorganised javascript into separate file

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,21 @@
-// scripts.js
-window.onload = function() {
-    document.querySelector('.navbar').style.backgroundColor = "#444";
-  };
-  
+var tablinks = document.getElementsByClassName("tab-links");
+var tabcontents = document.getElementsByClassName("tab-contents");
+function opentab(tabname) {
+  for (tablink of tablinks) {
+    tablink.classList.remove("active-link");
+  }
+  for (tabcontent of tabcontents) {
+    tabcontent.classList.remove("active-tab");
+  }
+  event.currentTarget.classList.add("active-link");
+  document.getElementById(tabname).classList.add("active-tab");
+}
+
+var sidemenu = document.getElementById("sidemenu");
+
+function openmenu(){
+  sidemenu.style.right = "0";
+}
+function closemenu(){
+  sidemenu.style.right = "-200px";
+}
